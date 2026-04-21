6f1d547e8516941bd950aa78ba3f04a637f3fcaa
kd1407336 <kd1407336@st.kobedenshi.ac.jp>
2026-04-21 14:45:35 +0900

    リザルト処理追加
    リザルト処理追加

--- Shooting/Src/Application/Result/Result.cpp
+++ Shooting/Src/Application/Result/Result.cpp
@@ -2,15 +2,22 @@
 
 void C_ResultScene::Draw()
 {
-
+	SHADER.m_spriteShader.SetMatrix(m_mat);
+	SHADER.m_spriteShader.DrawTex(&m_tex, Math::Rectangle(0, 0, 64, 64), 1.0f);
 }
 
 void C_ResultScene::Update()
 {
-
+	m_trans = Math::Matrix::CreateTranslation(m_pos.x, m_pos.y, 0);
+	m_scale = Math::Matrix::CreateScale(m_size.x, m_size.y, 0);
+	m_mat = m_scale * m_trans;
 }
 
 void C_ResultScene::Init()
 {
+	m_pos = { 0,0 };
+	m_size = { 1.0f,1.0f };
+
+	
 
 }
